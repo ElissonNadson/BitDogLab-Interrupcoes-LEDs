66a966706de0fdc78986c4f48ba5e128f08586f2
Elisson Marques <nadsonnodachi@gmail.com>
2025-02-02 23:20:07 -0300

    up

--- BitDogLab-Interrupcoes-LEDs.c
+++ BitDogLab-Interrupcoes-LEDs.c
@@ -1,130 +1,199 @@
-#include "pico/stdlib.h"
-#include "hardware/gpio.h"
-#include "hardware/irq.h"
-#include "pico/time.h"
+/******************************************************************************
+ * Bibliotecas
+ ******************************************************************************/
 #include <stdio.h>
-#include "ws2812.h" // Adicione esta linha
+#include <stdlib.h>
+#include "pico/stdlib.h"
+#include "hardware/pio.h"
+#include "hardware/clocks.h"
+#include "ws2812.pio.h"
 
-#define LED_WS2812_PIN 7
-#define BOTAO_A_PIN    5
-#define BOTAO_B_PIN    6
-#define DEBOUNCE_DELAY_MS 200
+/******************************************************************************
+ * Definições
+ ******************************************************************************/
+#define MATRIX 7
 #define NUM_PIXELS 25
+#define BUTTON_A 5
+#define BUTTON_B 6
+#define DEBOUNCE_DELAY 200 // Em milissegundos
+#define LED_RED 13
+#define LED_GREEN 11
+#define LED_BLUE 12
 
-// Matriz de números para exibição
-const double numeros[10][NUM_PIXELS] = {
+/******************************************************************************
+ * Números
+ ******************************************************************************/
+const double numbers[10][25] = {
     // Numero 0
     {0.0, 1.0, 1.0, 1.0, 0.0,
      1.0, 1.0, 0.0, 0.0, 1.0,
      1.0, 0.0, 1.0, 0.0, 1.0,
      1.0, 0.0, 0.0, 1.0, 1.0,
      0.0, 1.0, 1.0, 1.0, 0.0},
+
     // Numero 1
     {0.0, 0.0, 1.0, 0.0, 0.0,
-     0.0, 1.0, 1.0, 0.0, 0.0,
+     0.0, 0.0, 1.0, 1.0, 0.0,
      0.0, 0.0, 1.0, 0.0, 0.0,
      0.0, 0.0, 1.0, 0.0, 0.0,
      0.0, 1.0, 1.0, 1.0, 0.0},
+
     // Numero 2
-    {0.0, 1.0, 1.0, 1.0, 0.0,
+    {0.0, 0.0, 1.0, 1.0, 0.0,
+     1.0, 0.0, 0.0, 1.0, 0.0,
      0.0, 0.0, 0.0, 1.0, 0.0,
-     0.0, 1.0, 1.0, 1.0, 0.0,
-     0.0, 1.0, 0.0, 0.0, 0.0,
-     0.0, 1.0, 1.0, 1.0, 0.0},
+     0.0, 0.0, 1.0, 0.0, 0.0,
+     0.0, 1.0, 1.0, 1.0, 1.0},
+
     // Numero 3
     {0.0, 1.0, 1.0, 1.0, 0.0,
-     0.0, 0.0, 0.0, 1.0, 0.0,
-     0.0, 1.0, 1.0, 1.0, 0.0,
-     0.0, 0.0, 0.0, 1.0, 0.0,
-     0.0, 1.0, 1.0, 1.0, 0.0},
-    // Numero 4
-    {0.0, 0.0, 0.0, 1.0, 0.0,
+     1.0, 0.0, 0.0, 0.0, 1.0,
      0.0, 0.0, 1.0, 1.0, 0.0,
-     0.0, 1.0, 0.0, 1.0, 0.0,
-     1.0, 1.0, 1.0, 1.0, 1.0,
-     0.0, 0.0, 0.0, 1.0, 0.0},
+     1.0, 0.0, 0.0, 0.0, 1.0,
+     0.0, 1.0, 1.0, 1.0, 0.0},
+
+    // Numero 4
+    {0.0, 0.0, 1.0, 0.0, 0.0,
+     0.0, 0.0, 1.0, 1.0, 0.0,
+     1.0, 1.0, 1.0, 1.0, 0.0,
+     0.0, 0.0, 1.0, 0.0, 0.0,
+     0.0, 0.0, 1.0, 0.0, 0.0},
+
     // Numero 5
-    {0.0, 1.0, 1.0, 1.0, 0.0,
-     0.0, 1.0, 0.0, 0.0, 0.0,
-     0.0, 1.0, 1.0, 1.0, 0.0,
-     0.0, 0.0, 0.0, 1.0, 0.0,
-     0.0, 1.0, 1.0, 1.0, 0.0},
+    {1.0, 1.0, 1.0, 1.0, 1.0,
+     0.0, 0.0, 0.0, 0.0, 1.0,
+     1.0, 1.0, 1.0, 1.0, 1.0,
+     1.0, 0.0, 0.0, 0.0, 0.0,
+     1.0, 1.0, 1.0, 1.0, 1.0},
+
     // Numero 6
-    {0.0, 1.0, 1.0, 1.0, 0.0,
-     0.0, 1.0, 0.0, 0.0, 0.0,
-     0.0, 1.0, 1.0, 1.0, 0.0,
-     0.0, 1.0, 0.0, 1.0, 0.0,
-     0.0, 1.0, 1.0, 1.0, 0.0},
+    {1.0, 1.0, 1.0, 1.0, 0.0,
+     0.0, 0.0, 0.0, 0.0, 1.0,
+     1.0, 1.0, 1.0, 1.0, 1.0,
+     1.0, 0.0, 0.0, 0.0, 1.0,
+     1.0, 1.0, 1.0, 1.0, 1.0},
+
     // Numero 7
-    {0.0, 1.0, 1.0, 1.0, 0.0,
+    {1.0, 1.0, 1.0, 1.0, 1.0,
+     1.0, 0.0, 0.0, 0.0, 0.0,
      0.0, 0.0, 0.0, 1.0, 0.0,
      0.0, 0.0, 1.0, 0.0, 0.0,
-     0.0, 1.0, 0.0, 0.0, 0.0,
-     1.0, 1.0, 1.0, 1.0, 1.0},
+     0.0, 1.0, 0.0, 0.0, 0.0},
+
     // Numero 8
     {0.0, 1.0, 1.0, 1.0, 0.0,
-     0.0, 1.0, 0.0, 1.0, 0.0,
+     1.0, 0.0, 0.0, 0.0, 1.0,
      0.0, 1.0, 1.0, 1.0, 0.0,
-     0.0, 1.0, 0.0, 1.0, 0.0,
+     1.0, 0.0, 0.0, 0.0, 1.0,
      0.0, 1.0, 1.0, 1.0, 0.0},
+
     // Numero 9
-    {0.0, 1.0, 1.0, 1.0, 0.0,
-     0.0, 1.0, 0.0, 1.0, 0.0,
-     0.0, 1.0, 1.0, 1.0, 0.0,
-     0.0, 0.0, 0.0, 1.0, 0.0,
-     0.0, 1.0, 1.0, 1.0, 0.0}
-};
+    {1.0, 1.0, 1.0, 1.0, 1.0,
+     1.0, 0.0, 0.0, 0.0, 1.0,
+     1.0, 1.0, 1.0, 1.0, 1.0,
+     1.0, 0.0, 0.0, 0.0, 0.0,
+     1.0, 1.0, 1.0, 1.0, 1.0}};
 
-volatile int contador = 0;
-volatile absolute_time_t ultimo_debounce_a = {0};
-volatile absolute_time_t ultimo_debounce_b = {0};
+/******************************************************************************
+ * Variáveis Globais
+ ******************************************************************************/
+volatile int current_number = 0;
+volatile uint32_t last_debounce_time_a = 0;
+volatile uint32_t last_debounce_time_b = 0;
 
-void atualizar_display_matriz(int numero) {
-    printf("Exibindo número: %d\n", numero);
-    for (int i = 0; i < NUM_PIXELS; i++) {
-        if (numeros[numero][i]) {
-            ws2812_set_pixel(i, 255, 255, 255); // Branco
-        } else {
-            ws2812_set_pixel(i, 0, 0, 0); // Desligado
-        }
+/******************************************************************************
+ * Funções
+ ******************************************************************************/
+bool debounce(uint gpio, volatile uint32_t *last_debounce_time) {
+    uint32_t current_time = to_ms_since_boot(get_absolute_time());
+    if (current_time - *last_debounce_time > DEBOUNCE_DELAY) {
+        *last_debounce_time = current_time;
+        return true;
     }
-    ws2812_show();
+    return false;
 }
 
-void tratar_interrupcao_botao(uint gpio, uint32_t eventos) {
-    absolute_time_t agora = get_absolute_time();
-    if (gpio == BOTAO_A_PIN) {
-        if (absolute_time_diff_us(ultimo_debounce_a, agora) > (DEBOUNCE_DELAY_MS * 1000)) {
-            contador = (contador + 1) % 10;
-            ultimo_debounce_a = agora;
-            atualizar_display_matriz(contador);
-        }
-    } else if (gpio == BOTAO_B_PIN) {
-        if (absolute_time_diff_us(ultimo_debounce_b, agora) > (DEBOUNCE_DELAY_MS * 1000)) {
-            contador = (contador - 1 + 10) % 10;
-            ultimo_debounce_b = agora;
-            atualizar_display_matriz(contador);
-        }
+void update_matrix(const double *pattern, PIO pio, uint sm)
+{
+    uint32_t value;
+    for (int i = 0; i < NUM_PIXELS; i++)
+    {
+        int inverted_index = NUM_PIXELS - 1 - i;
+        value = ((uint32_t)(pattern[inverted_index] * 255) << 8);
+        pio_sm_put_blocking(pio, sm, value);
     }
 }
 
-int main() {
+void gpio_irq_handler(uint gpio, uint32_t events)
+{
+    if (gpio == BUTTON_A && (events & GPIO_IRQ_EDGE_FALL))
+    {
+        if (debounce(gpio, &last_debounce_time_a))
+        {
+            if (current_number < 9)
+            {
+                current_number++;
+            }
+        }
+    }
+    else if (gpio == BUTTON_B && (events & GPIO_IRQ_EDGE_FALL))
+    {
+        if (debounce(gpio, &last_debounce_time_b))
+        {
+            if (current_number > 0)
+            {
+                current_number--;
+            }
+        }
+    }
+}
+
+int main()
+{
     stdio_init_all();
-    ws2812_init(LED_WS2812_PIN);
-    atualizar_display_matriz(contador);
-    
-    gpio_init(BOTAO_A_PIN);
-    gpio_set_dir(BOTAO_A_PIN, GPIO_IN);
-    gpio_pull_up(BOTAO_A_PIN);
-    gpio_init(BOTAO_B_PIN);
-    gpio_set_dir(BOTAO_B_PIN, GPIO_IN);
-    gpio_pull_up(BOTAO_B_PIN);
-    
-    gpio_set_irq_enabled_with_callback(BOTAO_A_PIN, GPIO_IRQ_EDGE_FALL, true, &tratar_interrupcao_botao);
-    gpio_set_irq_enabled_with_callback(BOTAO_B_PIN, GPIO_IRQ_EDGE_FALL, true, &tratar_interrupcao_botao);
-    
-    while (1) {
-        sleep_ms(10);
+
+    // Inicializa LEDs RGB
+    gpio_init(LED_RED);
+    gpio_set_dir(LED_RED, GPIO_OUT);
+    gpio_init(LED_GREEN);
+    gpio_set_dir(LED_GREEN, GPIO_OUT);
+    gpio_init(LED_BLUE);
+    gpio_set_dir(LED_BLUE, GPIO_OUT);
+
+    uint32_t last_blink_time = 0;
+    bool led_state = false;
+
+    // Configuração para controlar a matriz de LEDs
+    PIO pio = pio0;
+    uint offset = pio_add_program(pio, &ws2812_program); // Substitua pio_matrix_program por ws2812_program
+    uint sm = pio_claim_unused_sm(pio, true);
+    ws2812_program_init(pio, sm, offset, MATRIX, 800000, false); // Adicione o argumento rgbw
+
+    // Configuração dos Botões
+    gpio_init(BUTTON_A);
+    gpio_set_dir(BUTTON_A, GPIO_IN);
+    gpio_pull_up(BUTTON_A);
+    gpio_set_irq_enabled_with_callback(BUTTON_A, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
+
+    gpio_init(BUTTON_B);
+    gpio_set_dir(BUTTON_B, GPIO_IN);
+    gpio_pull_up(BUTTON_B);
+    gpio_set_irq_enabled(BUTTON_B, GPIO_IRQ_EDGE_FALL, true);
+
+    while (true)
+    {
+        uint32_t current_time = to_ms_since_boot(get_absolute_time());
+
+        if (current_time - last_blink_time >= 200)
+        {
+            last_blink_time = current_time;
+            led_state = !led_state;
+            gpio_put(LED_RED, led_state);
+        }
+
+        update_matrix(numbers[current_number], pio, sm);
+        sleep_ms(100);
     }
+
     return 0;
 }
